088d8cb0c6fbb3d2a24aa6f4c5b1bb0485d24720
Nickolay Sheika <nickolai.sheika@alterplay.com>
2019-12-12 17:09:49 +0200

    beautiful additions reversed

--- Source/Message/MessageClient.swift
+++ Source/Message/MessageClient.swift
@@ -970,7 +970,7 @@ public class MessageClient {
         }
     }
     
-    func encryptionKey(spaceId: String) -> EncryptionKey {
+    private func encryptionKey(spaceId: String) -> EncryptionKey {
         var key = self.encryptionKeys[spaceId]
         if key == nil {
             key = EncryptionKey(spaceId: spaceId)
